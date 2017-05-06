d98df7401fde6744a2e47625f8164a2e6a243d0e
tfwio <tfwroble@gmail.com>
2017-05-06 15:17:58 -0500

    regress (back in the header class)

--- MidiArpeggiator.h
+++ MidiArpeggiator.h
@@ -28,7 +28,26 @@ class MidiArpeggiator {
 //  friend class IPlugBase;
 public:
 
-  MidiArpeggiator();
+  MidiArpeggiator()
+
+    : Bb(0.), Lb(0.), mBpm(120.)
+
+    , mArpIsOn(1)
+    , mSync(0)
+    , mKeyTrack(0) // bool
+
+    , mBeat(1), mDiv(16) //, mSpeed(0.) // int32
+    , mBeatLength(0)
+    , mNoteDurationOn(0), mNoteDurationOff(0)
+    , mNoteCount(0), mNoteIndex(-1)
+    , mKeyAmp(127)
+    , mFs(0)
+    , mModTime(0), mRealTime(0)
+
+    , mMidiQueue()
+    , mMidiNotes(0)
+  {
+  }
   ~MidiArpeggiator(){}
 
   bool IsEnabled() { return this->mArpIsOn; }
@@ -57,7 +76,27 @@ public:
 
   IMidiQueue *GetMidiQueue() { return &mMidiQueue; }
 
-  void ProcessMidi(IMidiMsg *pMsg, ITimeInfo *pTimeInfo, int pSamplePos);
+  void ProcessMidi(IMidiMsg *pMsg, ITimeInfo *pTimeInfo, int pSamplePos)
+  {
+    using mmt = IMidiMsg::EStatusMsg;
+
+    mmt stat = pMsg->StatusMsg();
+    if (!(stat == mmt::kNoteOn || stat == mmt::kNoteOff)) return;
+
+    GetMidiTime(pTimeInfo, pSamplePos);
+
+    int mNNL = int(calculate()); // new note length
+
+    // if (!is_loaded) return; // for another plug --- if no preset then skip processing...
+    int vel = pMsg->Velocity(), nn = pMsg->NoteNumber(), nOff = pMsg->mOffset;
+
+    if (((stat == mmt::kNoteOn) && vel == 0) || stat == mmt::kNoteOff)
+      mMidiNotes.remove(nn);
+    else
+      mMidiNotes.add(MidiNode(0, nn, vel, 0x0B, mKeyOffset));
+
+    mNoteCount = mMidiNotes.size();
+  }
 
   int GetNewNoteMod()
   {
@@ -67,7 +106,23 @@ public:
   }
   
   // will always increment mNoteIndex+1
-  int GetNewNoteIndex();
+  int GetNewNoteIndex()
+  {
+    mNoteIndex = (mNoteIndex + 1) % GetNewNoteMod();
+    switch (mMode)
+    {
+    case ARPMODE::PARAM_ARP_MODE_UP: return mNoteIndex; break;
+    case ARPMODE::PARAM_ARP_MODE_DN: return mNoteCount - mNoteIndex - 1; break;
+    case ARPMODE::PARAM_ARP_MODE_UD:
+      if (mNoteIndex < mNoteCount) return mNoteIndex;
+      else return (mNoteCount - ((mNoteIndex + 1) % mNoteCount) - 1);
+      break;
+    case ARPMODE::PARAM_ARP_MODE_DU:
+      if (mNoteIndex < mNoteCount) return (mNoteCount - (mNoteIndex % mNoteCount) - 1);
+      else return (mNoteIndex + 1) % mNoteCount;
+    }
+    return -1;
+  }
 
   // --------------------------------
   // This guy's job is to append new notes to IMidiQueue so that after its finished
@@ -76,7 +131,38 @@ public:
   // (*) in the main-audio-thread, we want to track when mod changes the... (mModTime)
   // (*) we want to add a poly mode eventually.
   // --------------------------------
-  void ProcessBuffer(int pSamplePos, int nFrames, ITimeInfo *pTimeInfo);
+  void ProcessBuffer(int pSamplePos, int nFrames, ITimeInfo *pTimeInfo)
+  {
+    mNoteCount = mMidiNotes.size(); // just checking (for re-use)
+    GetMidiTime(pTimeInfo, pSamplePos);
+
+    if (nFrames <= 0) return;
+
+    bool has_notes = mNoteCount > 0;
+    bool has_note_on = (mModTime + nFrames) >= mBeatLength;
+
+    if (has_note_on) // WTF is has_rest_on have to do with anything at this point? 
+    {
+      const int32 n_offset = IPMAX(0, IPMIN(mBeatLength - mModTime, nFrames - 1));
+      const int32 n_offset_off = n_offset + mNoteDurationOff;
+
+      if (has_notes)
+      {
+        int newNoteIndex = GetNewNoteIndex();
+        MidiNode node = mMidiNotes.at(newNoteIndex);
+        if (node.CanDo(mKeyOffset))
+        {
+          IMidiMsg nMsg = MakeNote(node, n_offset);
+          mMidiQueue.Add(&nMsg);
+
+          IMidiMsg nMsgOff = MakeNote(node, n_offset_off, false);
+          mMidiQueue.Add(&nMsgOff);
+        }
+      }
+    }
+
+    mModTime = (mModTime + nFrames) % mBeatLength;
+  }
 
   int32 getNoteDurationOn()  const { return mNoteDurationOn; }
   int32 getNoteDurationOff() const { return mNoteDurationOff; }
@@ -134,7 +220,18 @@ public:
 
 protected:
 
-  IMidiMsg MakeNote(MidiNode &node, int offset, bool isOn = true);
+  IMidiMsg MakeNote(MidiNode &node, int offset, bool isOn = true)
+  {
+    IMidiMsg nMsg = IMidiMsg(offset);
+    if (isOn) nMsg.MakeNoteOnMsg(
+      node.GetNote() + mKeyOffset,
+      (!mKeyTrack && isOn) ? mKeyAmp : node.GetAmp(),
+      offset,
+      0
+      );
+    else nMsg.MakeNoteOffMsg(node.GetNote() + mKeyOffset, offset, 0);
+    return nMsg;
+  }
 
 private:
   bool mArpIsOn, mSync, mKeyTrack;
@@ -159,106 +256,6 @@ private:
     , mSampleRate;
 };
 
-MidiArpeggiator::MidiArpeggiator()
 
-  : Bb(0.), Lb(0.), mBpm(120.)
 
-  , mArpIsOn(1)
-  , mSync(0)
-  , mKeyTrack(0) // bool
-
-  , mBeat(1), mDiv(16) //, mSpeed(0.) // int32
-  , mBeatLength(0)
-  , mNoteDurationOn(0), mNoteDurationOff(0)
-  , mNoteCount(0), mNoteIndex(-1)
-  , mKeyAmp(127)
-  , mFs(0)
-  , mModTime(0), mRealTime(0)
-
-  , mMidiQueue()
-  , mMidiNotes(0)
-{
-}
-int MidiArpeggiator::GetNewNoteIndex()
-{
-  mNoteIndex = (mNoteIndex + 1) % GetNewNoteMod();
-  switch (mMode)
-  {
-  case ARPMODE::PARAM_ARP_MODE_UP: return mNoteIndex; break;
-  case ARPMODE::PARAM_ARP_MODE_DN: return mNoteCount - mNoteIndex - 1; break;
-  case ARPMODE::PARAM_ARP_MODE_UD:
-    if (mNoteIndex < mNoteCount) return mNoteIndex;
-    else return (mNoteCount - ((mNoteIndex + 1) % mNoteCount) - 1);
-    break;
-  case ARPMODE::PARAM_ARP_MODE_DU:
-    if (mNoteIndex < mNoteCount) return (mNoteCount - (mNoteIndex % mNoteCount) - 1);
-    else return (mNoteIndex + 1) % mNoteCount;
-  }
-  return -1;
-}
-void MidiArpeggiator::ProcessMidi(IMidiMsg *pMsg, ITimeInfo *pTimeInfo, int pSamplePos)
-{
-  using mmt = IMidiMsg::EStatusMsg;
-
-  mmt stat = pMsg->StatusMsg();
-  if (!(stat == mmt::kNoteOn || stat == mmt::kNoteOff)) return;
-
-  GetMidiTime(pTimeInfo, pSamplePos);
-
-  int mNNL = int(calculate()); // new note length
-
-  // if (!is_loaded) return; // for another plug --- if no preset then skip processing...
-  int vel = pMsg->Velocity(), nn = pMsg->NoteNumber(), nOff = pMsg->mOffset;
-
-  if (((stat == mmt::kNoteOn) && vel == 0) || stat == mmt::kNoteOff)
-    mMidiNotes.remove(nn);
-  else
-    mMidiNotes.add(MidiNode(0, nn, vel, 0x0B, mKeyOffset));
-
-  mNoteCount = mMidiNotes.size();
-}
-void MidiArpeggiator::ProcessBuffer(int pSamplePos, int nFrames, ITimeInfo *pTimeInfo)
-{
-  mNoteCount = mMidiNotes.size(); // just checking (for re-use)
-  GetMidiTime(pTimeInfo, pSamplePos);
-
-  if (nFrames <= 0) return;
-
-  bool has_notes = mNoteCount > 0;
-  bool has_note_on = (mModTime + nFrames) >= mBeatLength;
-
-  if (has_note_on) // WTF is has_rest_on have to do with anything at this point? 
-  {
-    const int32 n_offset = IPMAX(0, IPMIN(mBeatLength - mModTime, nFrames - 1));
-    const int32 n_offset_off = n_offset + mNoteDurationOff;
-
-    if (has_notes)
-    {
-      int newNoteIndex = GetNewNoteIndex();
-      MidiNode node = mMidiNotes.at(newNoteIndex);
-      if (node.CanDo(mKeyOffset))
-      {
-        IMidiMsg nMsg = MakeNote(node, n_offset);
-        mMidiQueue.Add(&nMsg);
-
-        IMidiMsg nMsgOff = MakeNote(node, n_offset_off, false);
-        mMidiQueue.Add(&nMsgOff);
-      }
-    }
-  }
-
-  mModTime = (mModTime + nFrames) % mBeatLength;
-}
-IMidiMsg MidiArpeggiator::MakeNote(MidiNode &node, int offset, bool isOn)
-{
-  IMidiMsg nMsg = IMidiMsg(offset);
-  if (isOn) nMsg.MakeNoteOnMsg(
-    node.GetNote() + mKeyOffset,
-    (!mKeyTrack && isOn) ? mKeyAmp : node.GetAmp(),
-    offset,
-    0
-    );
-  else nMsg.MakeNoteOffMsg(node.GetNote() + mKeyOffset, offset, 0);
-  return nMsg;
-}
 #endif
